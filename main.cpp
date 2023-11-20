835c5f1a43121f942de0ce186b91744a316316da
VitoS <ikn0@proton.me>
2023-11-20 22:40:04 +0100

    eingabeM implementiert

--- main.cpp
+++ main.cpp
@@ -40,6 +40,40 @@ public:
     void spielerEingabe(Spieler spieler) {
         // Methodencode
     }
+
+    bool pruefen(int zahl, int zahlenSize) {
+        if (zahl >= 1 && zahl <= zahlenSize) {
+            return true; // Die Zahl ist gültig
+        } else {
+            return false; // Die Zahl ist ungültig
+        }
+    }
+
+    void eingabeM(int zahlenSize, int zahlenRange, int ausgabeBreite) {
+        cout << "Geben Sie " << zahlenSize << " Werte ein (1-" << zahlenRange << "):" << endl;
+        bool istEinzigartig;
+        int zahl = 0;
+        vector<int> zahlen(zahlenSize);
+        for (int i = 0; i < zahlenSize; i++) {
+            do {
+                cin >> zahl;
+                istEinzigartig = true;
+                for (int j = 0; j < i; j++) {
+                    if (!pruefen(zahl, zahlenRange)) {
+                        istEinzigartig = false;
+                        cout << "Ungültige Eingabe! Bitte geben Sie eine Zahl zwischen 1 und " << zahlenRange << " ein." << endl;
+                    }
+                    if (zahlen[j] == zahl) {
+                        istEinzigartig = false;
+                        break;
+                    }
+                }
+            } while (!istEinzigartig);
+            zahlen[i] = zahl;
+        }
+        
+    }
+
     void ziehung() {
         // Methodencode
     }
@@ -52,5 +86,6 @@ private:
 };
 
 int main(){
-  cout << "hello";
+  Lotto lotto;
+  lotto.eingabeM(6,49,7);
 }
