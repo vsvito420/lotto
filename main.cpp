b6ecb859cf95dc550233c9d980772e789f0f848d
VitoS <ikn0@proton.me>
2023-11-20 12:52:04 -0800

    colors

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std:
+#define cRST  "\033[0m"       // Reset
+#define cBLK  "\033[30m"      // Schwarz
+#define cRED  "\033[31m"      // Rot
+#define cGRN  "\033[32m"      // Grün
+#define cYLW  "\033[33m"      // Gelb
+#define cBLU  "\033[34m"      // Blau
+#define cMGT  "\033[35m"      // Magenta
+#define cCYN  "\033[36m"      // Cyan
+#define cWHT  "\033[37m"      // Weiß
 
 int main(){
   cout << "hello";
