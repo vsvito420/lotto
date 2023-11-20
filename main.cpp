52e384ae4e0ac6abb7bd289c7553ef319ef537be
VitoS <ikn0@proton.me>
2023-11-20 22:22:03 +0100

    syntax fehler behoben

--- main.cpp
+++ main.cpp
@@ -1,49 +1,52 @@
 #include <iostream>
-using namespace std:
-#define cRST  "\033[0m"       // Reset
-#define cBLK  "\033[30m"      // Schwarz
-#define cRED  "\033[31m"      // Rot
-#define cGRN  "\033[32m"      // Grün
-#define cYLW  "\033[33m"      // Gelb
-#define cBLU  "\033[34m"      // Blau
-#define cMGT  "\033[35m"      // Magenta
-#define cCYN  "\033[36m"      // Cyan
-#define cWHT  "\033[37m"      // Weiß
+#include <vector>
+using namespace std;
 
-class Spiel { 
-/* Die Spiel-Klasse hat Methoden zum Setzen und Abrufen 
-der gezogenen Zahlen sowie eine Methode, um zu prüfen, ob ein Spieler gewonnen hat.*/
+class Spiel {
 public:
-    void setZahlen(vector<int> zahlen);
-    vector<int> getZahlen();
-    bool gewonnen();
+    void setZahlen(vector<int> zahlen) {
+        // Methodencode
+    }
+    vector<int> getZahlen() {
+        // Methodencode
+    }
+    bool gewonnen() {
+        // Methodencode
+    }
 private:
     vector<int> gezogeneZahlen;
 };
 
 class Spieler {
-/*Die Spieler-Klasse hat Methoden zum Setzen und Abrufen des Gewinnstatus eines Spielers.*/
 public:
-    void setGewonnen(bool gewonnen);
-    bool getGewonnen();
+    void setGewonnen(bool gewonnen) {
+        // Methodencode
+    }
+    bool getGewonnen() {
+        // Methodencode
+    }
 private:
     bool gewonnen;
 };
 
 class Lotto {
-/*Die Lotto-Klasse hat Methoden zum Erstellen neuer Spiele, zum Sammeln von Spieler-Eingaben, 
-zur Ziehung der Lottozahlen und zum Anzeigen des Ergebnisses.*/
 public:
-    void neuesSpiel();
-    void spielerEingabe(Spieler spieler);
-    void ziehung();
-    void ergebnis();
+    void neuesSpiel() {
+        // Methodencode
+    }
+    void spielerEingabe(Spieler spieler) {
+        // Methodencode
+    }
+    void ziehung() {
+        // Methodencode
+    }
+    void ergebnis() {
+        // Methodencode
+    }
 private:
     vector<Spiel> spiele;
     vector<Spieler> spieler;
 };
-
-
 
 int main(){
   cout << "hello";
