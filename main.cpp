a792d8227026f316709c91b16185e79d6651456d
VitoS <ikn0@proton.me>
2023-11-21 17:30:13 +0100

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@
 using namespace std;
 
 struct Zahl {
-    int zahl;
+    int number;
     int count;
 };
 
 int main() {
     vector<Zahl> vector;
-    Zahl p1 = {1, 0};
-    Zahl p2 = {2, 0};
-    vector.push_back(p1);
-    vector.push_back(p2);
-    for (auto& v : vector) {
-        cout << "Zahl: " << v.zahl << ", Anzahl: " << v.count << endl;
+
+    for (int i = 1; i <= 5; ++i) {
+        Zahl newZahl = {i, 0};
+        vector.push_back(newZahl);
     }
+
+    for (auto& v : vector) {
+        cout << "Zahl: " << v.number << ", Anzahl: " << v.count << endl;
+    }
+    
     return 0;
 }
