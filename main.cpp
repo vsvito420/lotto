1690b1e38e7245cb2b4156cef4c7fb9a22698591
VitoS <ikn0@proton.me>
2023-11-22 00:14:38 +0100

    updateZahlen added

--- main.cpp
+++ main.cpp
@@ -1,35 +1,44 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define cRST  "\033[0m"       // Reset
-#define cRED  "\033[31m"      // Rot
+#define colorRST  "\033[0m"       // Reset
+#define colorRED  "\033[31m"      // Rot
 
-class Lotto {
+class C_LoteryTicket {
 public:
-    struct Zahl {
-        int number;
-        int count;
+    struct s_element { // schein element , eine zahl und eine anzahl
+        int i_zahl;
+        int i_anzahl;
     };
 
-    void schein(int scheinRange, int scheinBreite) {
-        vector<Zahl> vector;
+    void m_schein(int scheinRange, int scheinBreite) {
+        vector<s_element> vec;
 
         for (int i = 1; i <= scheinRange; ++i) {
-            Zahl newZahl = {i, 0};
-            vector.push_back(newZahl);
+            s_element newZahl = {i, 0};
+            vec.push_back(newZahl);
         }
 
-        int j=0;
-        for (size_t i = 0; i < vector.size(); ++i) {
+        int j=0; // bugfix: use of j intead of i did the job
+        for (size_t i = 0; i < vec.size(); ++i) {
             j++;
-            cout << cRED << vector[i].count << "x " << cRST << vector[i].number << "\t";
+            cout << colorRED << vec[i].i_anzahl << "x " << colorRST << vec[i].i_zahl << "\t";
             if (j % scheinBreite == 0) {
                 cout << endl;
             } else {
                 continue;
             }
         }
-        cout << endl << vector[5].count;
+        cout << endl << vec[5].i_anzahl;
+    }
+
+    void updateZahlen(vector<s_element>& vec, int slot, int wert) {
+        for (size_t i = 0; i < vec.size(); ++i) {
+            if (vec[i].i_zahl == slot) {
+                vec[i].i_anzahl = wert;
+                break;
+            }
+        }
     }
 
 private:
@@ -37,9 +46,13 @@ private:
 };
 
 int main() {
-    Lotto lotto;
-    lotto.schein(49,7);
-    
+    C_LoteryTicket lotto;
+    lotto.m_schein(49,7);
 
+    // Beispiel: Aktualisiere die Zahl 5 mit dem Wert 3
+    int slot = 5;
+    int wert = 3;
+    lotto.updateZahlen(slot, wert);
+    
     return 0;
 }
