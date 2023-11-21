932cec9b117894e36cb2a3c18b257bef6e50aa73
VitoS <ikn0@proton.me>
2023-11-21 17:53:30 +0100

    modified:   main.cpp 	modified:   main.exe

--- main.cpp
+++ main.cpp
@@ -9,16 +9,18 @@ struct Zahl {
     int count;
 };
 
+
+
 int main() {
     vector<Zahl> vector;
 
-    for (int i = 1; i <= 5; ++i) {
+    for (int i = 1; i <= 49; ++i) {
         Zahl newZahl = {i, 0};
         vector.push_back(newZahl);
     }
 
     for (auto& v : vector) {
-        cout << "Zahl: " << v.number << ", Anzahl: " << v.count << endl;
+        cout << "Zahl: " << v.number << ", \tAnzahl: " << v.count << endl;
     }
     
     return 0;
