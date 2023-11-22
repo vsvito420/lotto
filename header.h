d0dae09465ab35b7e5f021ff33ea533368516813
VitoS <ikn0@proton.me>
2023-11-23 00:55:37 +0100

    deleted:    header.h 	deleted:    main.exe 	renamed:    C_LoteryTicket.h -> new.cpp 	new file:   new.exe     deleted:    new2.exe

--- header.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-#include "C_LoteryTicket.h" // Stellen Sie sicher, dass Ihre C_LoteryTicket-Klasse in einer Header-Datei definiert ist
-#include <limits>
-#include <map>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
