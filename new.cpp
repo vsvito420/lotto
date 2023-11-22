e6e7b2f486fb0e9d7838a60842c953d8cfd489b2
VitoS <ikn0@proton.me>
2023-11-22 16:16:37 +0100

    old version

--- new.cpp
+++ new.cpp
@@ -1,127 +1,66 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-#define color0  "\033[0m"       // Reset
-#define color1  "\033[31m"      // Rot
-#define color2  "\033[32m"      // 
-#define color3  "\033[33m"      // 
-#define color4  "\033[34m"      // 
-#define color5  "\033[35m"      // 
-#define color6  "\033[36m"      // 
 
-class C_LoteryGen{
+class LotteryTicket {
 public:
-    bool pruefen(int zahl, int zahlenSize) {
-        if (zahl >= 1 && zahl <= zahlenSize) {
-            return true; // Die Zahl ist gültig
-        } else {
-            return false; // Die Zahl ist ungültig
-        }
-    }
-    
-    void eingabeM(int zahlenSize, int zahlenRange, int ausgabeBreite) {
-        cout << "Geben Sie " << zahlenSize << " Werte ein (1-" << zahlenRange << "):" << endl;
-        bool istEinzigartig;
-        int zahl = 0;
-        int zahlen[zahlenSize];
-        for (int i = 0; i < zahlenSize; i++) {
-            do {
-                cin >> zahl;
-                istEinzigartig = true;
-                for (int j = 0; j < i; j++) {
-                    if (!pruefen(zahl, zahlenRange)) {
-                        istEinzigartig = false;
-                        cout << "Ungültige Eingabe! Bitte geben Sie eine Zahl zwischen 1 und " << zahlenRange << " ein." << endl;
-                    }
-                    if (zahlen[j] == zahl) {
-                        istEinzigartig = false;
-                        break;
-                    }
-                }
-            } while (!istEinzigartig);
-            zahlen[i] = zahl;
-        }
-        
-    }
-};
-class C_LoteryTicket {
-public:
-    
-    struct s_element { // schein element , eine zahl und eine anzahl
-        int i_zahl;
-        int i_anzahl;
+    struct Element {
+        int number;
+        int count;
     };
-    
-    vector<s_element> vector;
-    
-    void m_zahlEingeben(int i, int temp){
-        s_element newZahl = {i, temp};
-        vector.push_back(newZahl);
-    }
 
-    bool pruefen(int zahl, int zahlenSize) {
-        if (zahl >= 1 && zahl <= zahlenSize) {
-            return true; // Die Zahl ist gültig
-        } 
-        else{
-            return false; // Die Zahl ist ungültig
-        }       
-    }
+private:
+    vector<Element> elements;
 
-    void m_Tippschein(int zahlenSize, int scheinRange, int ausgabeBreite){
-        bool IstEinzigartig=0;
-        int zahl = 0;
-        cout << "Geben Sie " << zahlenSize << " Werte ein (1-" << scheinRange << "):" << endl;
-        for (int i = 0; i < zahlenSize; i++) {
-            do {
-                cin >> zahl;
-                IstEinzigartig = true;
-                for (int j = 0; j < i; j++) {
-                    if (!pruefen(zahl, scheinRange)) {
-                        IstEinzigartig = false;
-                        cout << "Ungültige Eingabe! Bitte geben Sie eine Zahl zwischen 1 und " << scheinRange << " ein." << endl;
-                    }
-                    if (vector[j].i_zahl == zahl) {
-                        cout << color3 << vector[j].i_zahl;
-                        IstEinzigartig = false;
-                        break;
-                    }
-                }
-            } while (!IstEinzigartig);
-            s_element newZahl = {zahl, 1}; // Add this line to create a new element with the entered number and an count of 1
-            vector.push_back(newZahl); // Add this line to add the new element to the vector
+public:
+    bool isValidNumber(int number, int maxNumber) {
+        if (number >= 1 && number <= maxNumber) {
+            return true;
+        } else {
+            return false;
         }
     }
 
-    void m_scheinPrint(int scheinBreite) {
-        cout << color2 << "+--------------------o_lottoschein------------------------+" << color0 << endl;
-        int j=0; // bugfix: use of j intead of i did the job
-        for (size_t i = 0; i < vector.size(); ++i) {
-            j++;
-            cout << color2<< "|" << color0 ;
-            cout << color1 << vector[i].i_anzahl << "x " << color0 << vector[i].i_zahl << "\t";
-            if (j % scheinBreite == 0) {
-                cout << color2<< "|" << color0 ;
-                cout << endl;
+    void enterNumber(int number, int maxNumber) {
+        bool isUnique = true;
+        for (size_t i = 0; i < elements.size(); i++) {
+            if (!isValidNumber(number, maxNumber)) {
+                isUnique = false;
+                cout << "Invalid input! Please enter a number between 1 and " << maxNumber << "." << endl;
+            }
+            if (elements[i].number == number) {
+                isUnique = false;
+                break;
+            }
+        }
+
+        if (isUnique) {
+            Element newNumber = {number, 1};
+            elements.push_back(newNumber);
+        }
+    }
+
+    void printTicket(int ticketWidth) {
+        cout << "+--------------------o_lottoschein------------------------+" << endl;
+        int counter = 0;
+        for (size_t i = 0; i < elements.size(); ++i) {
+            counter++;
+            cout << "|" << elements[i].count << "x " << elements[i].number << "\t";
+            if (counter % ticketWidth == 0) {
+                cout << "|" << endl;
             } else {
-                
                 continue;
             }
         }
-        cout << color2 << "+-------------------------------------------------------+" << color0 << endl;
+        cout << "+-------------------------------------------------------+" << endl;
     }
-
-private:
-
 };
 
 int main() {
-    C_LoteryTicket o_lotto;
-    // o_lotto.m_zahlEingeben(2,6);
-    o_lotto.m_Tippschein(6,49,7);
-    o_lotto.m_scheinPrint(7);
+    LotteryTicket lotto;
+    lotto.enterNumber(2, 6);
+    lotto.printTicket(7);
     cout << endl;
-    
 
     return 0;
 }
