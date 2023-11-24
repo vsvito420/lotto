c955f3d0191e508c3dc38d16dc7a246c7f07ba06
VitoS <ikn0@proton.me>
2023-11-24 01:19:39 +0100

    working 0 termininate

--- main.cpp
+++ main.cpp
@@ -6,186 +6,254 @@
 #include <ctime>
 using namespace std;
 
-#define color0  "\033[0m"       // Reset
-#define color1  "\033[31m"      // Rot
-#define color2  "\033[32m"      // Grün
-#define color3  "\033[33m"      // Gelb
-#define color4  "\033[34m"      // Blau
-#define color5  "\033[35m"      // Magenta
-#define color6  "\033[36m"      // Cyan
+#define color0 "\033[0m"  // Reset
+#define color1 "\033[31m" // Rot
+#define color2 "\033[32m" // Grün
+#define color3 "\033[33m" // Gelb
+#define color4 "\033[34m" // Blau
+#define color5 "\033[35m" // Magenta
+#define color6 "\033[36m" // Cyan
 
-class C_LoteryTicket {
+class Ticket
+{
 public:
-    struct s_element {
-        int i_zahl;
-        int i_anzahl;
+    int t_Size;
+    int t_Range;
+
+    struct cell
+    {
+        int cellNumber;
+        int cellAmount;
     };
 
-    const vector<s_element>& getVector() const { return vector; }
+    const vector<cell> &getVector() const { return vector; }
 
-    bool pruefen(int zahl, int zahlenSize) const {
-        return zahl >= 1 && zahl <= zahlenSize;
+    bool pruefen(int zahl, int t_Size) const
+    {
+        return zahl >= 1 && zahl <= t_Size;
     }
 
-    void input(int zahlenSize, int scheinRange, bool autoGenerate = false){
-        bool IstEinzigartig=0;
+    void input(int t_Size, int t_Range, bool autoGenerate = false)
+    {
+        bool IstEinzigartig = 0;
         int zahl = 0;
-        cout << "Geben Sie " << zahlenSize << " Werte ein (1-" << scheinRange << "):" << endl;
-        for (int i = 0; i < zahlenSize; i++) {
-            do {
-                if (autoGenerate) {
-                    zahl = (rand() % scheinRange) + 1;
-                } else {
+        cout << "Geben Sie " << t_Size << " Werte ein (1-" << t_Range << "):" << endl;
+        for (int i = 0; i < t_Size; i++)
+        {
+            do
+            {
+                if (autoGenerate)
+                {
+                    zahl = (rand() % t_Range) + 1;
+                }
+                else
+                {
                     cin >> zahl;
                 }
                 IstEinzigartig = true;
-                for (int j = 0; j < i; j++) {
-                    if (!pruefen(zahl, scheinRange)) {
+                for (int j = 0; j < i; j++)
+                {
+                    if (!pruefen(zahl, t_Range))
+                    {
                         IstEinzigartig = false;
-                        cout << "Ungültige Eingabe! Bitte geben Sie eine Zahl zwischen 1 und " << scheinRange << " ein." << endl;
+                        cout << "Ungültige Eingabe! Bitte geben Sie eine Zahl zwischen 1 und " << t_Range << " ein." << endl;
                     }
-                    if (vector[j].i_zahl == zahl) {
-                        cout << "ERROR: " << color3 << vector[j].i_zahl << color2 << " Wurde bereits gewählt!" << color5 << endl;
+                    if (vector[j].cellNumber == zahl)
+                    {
+                        cout << "ERROR: " << color3 << vector[j].cellNumber << color2 << " Wurde bereits gewählt!" << color5 << endl;
                         IstEinzigartig = false;
                         break;
                     }
                 }
             } while (!IstEinzigartig);
-            s_element newZahl = {zahl, 1}; 
-            vector.push_back(newZahl); 
+            cell newZahl = {zahl, 1};
+            vector.push_back(newZahl);
         }
     }
 
-    void output() {
-        cout << color2 << "+-------------------------------------------------------+" << color0 << endl;
+    void output()
+    {
+        
+        cout << color2 << "_______" << color0 << endl;
+        
         map<int, int> countMap;
-        for (auto& el : vector) {
-            countMap[el.i_zahl] = el.i_anzahl;
+        for (auto &el : vector)
+        {
+            countMap[el.cellNumber] = el.cellAmount;
         }
-        for (int i = 1; i <= 49; ++i) {
-            cout << color2<< "|" << color0 ;
-            if (countMap[i] == 0) {
-                cout << color6 << countMap[i] << "x " << color0 << i << "\t";
-            } else {
-                cout << color1 << countMap[i] << "x " << color0 << i << "\t";
+        for (int i = 1; i <= 49; ++i)
+        {
+            cout << color2 << "|" << color0;
+            if (countMap[i] == 0)
+            {
+                cout << color1 << countMap[i] << "x " << color0;
             }
-            if (i % 7 == 0) {
-                cout << color2<< "|" << color0 ;
+            else
+            {
+                cout << color3 << countMap[i] << "x " << color0;
+            }
+            if (i < 10) {
+                cout << "0"; // Add leading zero for single-digit numbers
+            }
+            cout << i;
+            if (i % 7 == 0)
+            {
+                cout << color2 << "|" << color0;
                 cout << endl;
             }
         }
         cout << color2 << "+-------------------------------------------------------+" << color0 << endl;
     }
 
-    void overlay(C_LoteryTicket& other) {
+    void overlay(Ticket &other)
+    {
         map<int, int> countMap;
-        for (auto& el : vector) {
-            countMap[el.i_zahl] += el.i_anzahl;
+        for (auto &el : vector)
+        {
+            countMap[el.cellNumber] += el.cellAmount;
         }
-        for (auto& el : other.getVector()) {
-            countMap[el.i_zahl] += el.i_anzahl;
+        for (auto &el : other.getVector())
+        {
+            countMap[el.cellNumber] += el.cellAmount;
         }
         vector.clear();
-        for (auto& pair : countMap) {
+        for (auto &pair : countMap)
+        {
             vector.push_back({pair.first, pair.second});
         }
     }
 
 private:
-    vector<s_element> vector;
+    vector<cell> vector;
 };
 
-void inputAuto(C_LoteryTicket& ticket) {
+void inputAuto(Ticket &ticket, int t_Size, int t_Range)
+{
     int choice;
-    cout << "Möchten Sie die Zahlen selbst eingeben oder generieren lassen?" << endl << "(1 - Selbst eingeben, 2 - Generieren lassen): ";
+    cout << "Möchten Sie die Zahlen selbst eingeben oder generieren lassen?" << endl
+         << "(1 - Selbst eingeben, 2 - Generieren lassen): ";
     cin >> choice;
-    if (choice == 1) {
-        ticket.input(6,49);
-    } else {
-        ticket.input(6,49, true);
+    if (choice == 1)
+    {
+        ticket.input(6, 49);
+    }
+    else
+    {
+        ticket.input(6, 49, true);
     }
     ticket.output();
     cout << endl;
 }
 
-int main() {
-    srand(time(0)); // Initialisieren Sie den Zufallsgenerator
+void start(int t_Size, int t_Range, bool autoGenerate = false)
+{
+    int numTickets;
+    while (true)
+    {
+        cout << "Wie viele Lottoscheine möchten Sie erstellen? ";
+        cin >> numTickets;
+        if (cin.fail() || numTickets <= 0)
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Ungültige Eingabe. Bitte geben Sie eine positive Zahl ein." << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
 
+    vector<Ticket> tickets(numTickets);
+    cout << "Möchten Sie die Lottoscheine generieren oder manuell eingeben?\n";
+    cout << "1. Generieren\n";
+    cout << "2. Manuell eingeben\n";
+
+    int auswahl2;
+    cin >> auswahl2;
+
+    switch (auswahl2)
+    {
+    case 1:
+    {
+        for (int i = 0; i < numTickets; i++)
+        {
+            tickets[i].input(t_Size, t_Range, true);
+        }
+        break;
+    }
+
+    case 2:
+    {
+        for (int i = 0; i < numTickets; i++)
+        {
+            tickets[i].input(t_Size, t_Range);
+        }
+        break;
+    }
+
+    default:
+    {
+        cout << "Ungültige Auswahl. Bitte wählen Sie eine gültige Option.\n";
+    }
+    }
+
+    cout << endl
+         << "Alle Lottoscheine Anzahl+Zahlen: " << endl;
+    for (int i = 1; i < numTickets; i++)
+    {
+        tickets[0].overlay(tickets[i]);
+    }
+    tickets[0].output();
+}
+
+void menuStartPage()
+{
     int auswahl;
-    do {
-        cout << "Bitte wählen Sie eine Option:\n";
+    do
+    {
+        cout << "! wählen Sie eine Option:\n";
         cout << "1. Lotto 6 aus 49\n";
         cout << "2. Eurolotto\n";
         cout << "3. Ende des Programmes\n";
         cin >> auswahl;
 
-        switch (auswahl) {
-            case 1: {
-                int numTickets;
-                while (true) {
-                    cout << "Wie viele Lottoscheine möchten Sie erstellen? ";
-                    cin >> numTickets;
-                    if (cin.fail() || numTickets <= 0) {
-                        cin.clear();
-                        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-                        cout << "Ungültige Eingabe. Bitte geben Sie eine positive Zahl ein." << endl;
-                    } else {
-                        break;
-                    }
-                }
+        switch (auswahl)
+        {
+        case 1:
+        {
+            Ticket lotto_649;
+            start(6, 49);
+            break;
+        }
 
-                vector<C_LoteryTicket> tickets(numTickets);
-                cout << "Möchten Sie die Lottoscheine generieren oder manuell eingeben?\n";
-                cout << "1. Generieren\n";
-                cout << "2. Manuell eingeben\n";
-                
-                int auswahl2;
-                cin >> auswahl2;
-                switch (auswahl2) {
-                    case 1: {
-                        for (int i = 0; i < numTickets; i++) {
-                            tickets[i].input(6, 49, true);
-                        }
-                        break;
-                    }
+        case 2:
+        {
+            Ticket lotto_550;
+            start(6, 49);
+            Ticket lotto_212;
+            start(6, 49);
+            break;
+        }
 
-                    case 2: {
-                        for (int i = 0; i < numTickets; i++) {
-                            tickets[i].input(6, 49);
-                        }
-                        break;
-                    }
-                    
-                    default: {
-                        cout << "Ungültige Auswahl. Bitte wählen Sie eine gültige Option.\n";
-                    }
-                }
+        case 3:
+        {
+            cout << "Programm beendet.\n";
+            break;
+        }
 
-                cout << endl << "Alle Lottoscheine Anzahl+Zahlen: " << endl;
-                for (int i = 1; i < numTickets; i++) {
-                    tickets[0].overlay(tickets[i]);
-                }
-                tickets[0].output();
-                break;
-            }
-
-            case 2: {
-                C_LoteryTicket eurolotto;
-                eurolotto.input(5, 50);
-                // Hier können Sie weitere Operationen mit dem Eurolotto-Schein durchführen
-                break;
-            }
-
-            case 3:{
-                cout << "Programm beendet.\n";
-                break;
-            }
-            
-            default:{
-                cout << "Ungültige Auswahl. Bitte wählen Sie eine gültige Option.\n";
-            }
+        default:
+        {
+            cout << "Ungültige Auswahl. Bitte wählen Sie eine gültige Option.\n";
+        }
         }
     } while (auswahl != 3);
+}
+
+int main()
+{
+    srand(time(0));  // Initialisieren Sie den Zufallsgenerator
+    menuStartPage(); // Starten Sie das Hauptmenü
 
     return 0;
 }
