6a701c877ba368ce1038ffbb10d350af9b6384e2
VitoS <ikn0@proton.me>
2023-11-26 23:34:08 +0100

    ...

--- new.cpp
+++ new.cpp
@@ -1,26 +1,29 @@
 #include <iostream> // cout, cin, endl
 #include <vector>   // vector
-#include <map>      // map
 using namespace std;
+
+struct LottoTableCell
+{
+    int lottoTableCellNumber;
+    int lottoTableCellAmount;
+};
 
 class Table
 {
 public:
+    void hinzufuegenZahl(int nummer, int anzahl) {
+        LottoTableCell zelle {nummer, anzahl};
+        lottoTableVector.push_back(zelle);
+    }
+
     void anzeigenLottoZahlen() {
         for(const auto& zelle : lottoTableVector) {
             cout << "Lottozahl: " << zelle.lottoTableCellNumber << ", Anzahl: " << zelle.lottoTableCellAmount << endl;
         }
     }
 
-protected: // protected, weil die Klasse Table von der Klasse Program erweitert wird    
-    map<int, int> lottoTableMap;
-    struct lottoTableCell
-    {
-        int lottoTableCellNumber;
-        int lottoTableCellAmount;
-    };
-
-    vector<lottoTableCell> lottoTableVector;
+protected:   
+    vector<LottoTableCell> lottoTableVector;
 };
 
 class Program : public Table
@@ -28,6 +31,12 @@ class Program : public Table
 public:
     void run() {
         // Hier können Sie Methoden der Table-Klasse aufrufen, z.B.
+        hinzufuegenZahl(7, 1);
+        hinzufuegenZahl(14, 1);
+        hinzufuegenZahl(21, 1);
+        hinzufuegenZahl(28, 1);
+        hinzufuegenZahl(35, 1);
+        hinzufuegenZahl(42, 1);
         anzeigenLottoZahlen();
     }
 };
@@ -36,6 +45,5 @@ int main()
 {
     Program programm;
     programm.run();
-    cout << "Programm beendet." << endl;
     return 0;
 }
