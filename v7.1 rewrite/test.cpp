c14d3223604944975a93eea0662f50e1c20ffa20
VitoS <ikn0@proton.me>
2024-02-01 20:05:56 +0100

    Refactor Lotto class to allow for customizable range of numbers

--- v7.1 rewrite/test.cpp
+++ v7.1 rewrite/test.cpp
@@ -1,35 +1,52 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <map>  // ich benutze map anstatt map da ich die gezogenen zahlen später zählen will
+#include <iomanip>  // für setw() und setfill()
 using namespace std;
 
 class Lotto {
 private:
     map<int, int> paar;
+    int darstellungsBreite = 7;
 
 public:
     // Konstruktor
     Lotto() {
+        int rangeStart = 0;
+        int rangeEnd = 0;
         // Elemente hinzufügen
-        paar[1] = 1;
-        paar[2] = 2;
-        paar[3] = 3;
+        for (int i = rangeStart; i <= rangeEnd; i++) {
+            paar[i] = 0;
+        }
     }
 
+    // Parameter Konstruktor
+    Lotto(int rangeStart, int rangeEnd) {
+        // Elemente hinzufügen
+        for (int i = rangeStart; i <= rangeEnd; i++) {
+            paar[i] = 0;
+        }
+    }
     // Funktion zum Ausgeben der Paare
-    void printPaar() {
+    void print() {
+        int zeilenumbruch = 0;  // Zähler für die neue Zeile
         for (const auto& pair : paar) {
-            cout << pair.first << ": " << pair.second << endl;
+            cout << "|" << setfill('0') << setw(2) << pair.first << " = " << setw(2) << pair.second << "  ";
+            zeilenumbruch++;
+            if (zeilenumbruch == darstellungsBreite) {
+                cout << endl;
+                zeilenumbruch = 0;
+            }
         }
     }
 };
 
 int main() {
     // Erstellen Sie ein Lotto-Objekt
-    Lotto lotto;
+    Lotto lotto(1, 49);
 
     // Paare ausgeben
-    lotto.printPaar();
+    lotto.print();
 
     return 0;
 }
