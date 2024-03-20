c4a8abd667adf91375c2b62e772e62a40728550c
VitoS <ikn0@proton.me>
2024-03-20 22:30:40 +0100

    kommentare

--- 20-03-2024/lotto-20-03_version-1.cpp
+++ 20-03-2024/lotto-20-03_version-1.cpp
@@ -1,131 +1,131 @@
-#include <iostream> // cout, cin
-#include <ctime>    // time
-#include <cstdlib>  // srand und rand
-#include <iomanip>  // setw
-using namespace std;
+#include <iostream>         // cout, cin
+#include <ctime>            // time
+#include <cstdlib>          // srand und rand
+#include <iomanip>          // setw
+using namespace std;        // std:: fällt weg
 
-class LottoGame
+class LottoGame             // Klasse für das Lotto Spiel
 {
 private:
-    int *numbers;
-    int *winningNumbers;
-    int numCount;
+    int *numbers;           // Array für die Zahlen des Spielers
+    int *winningNumbers;    // Array für die Gewinnzahlen
+    int numCount;           // Anzahl der Zahlen des Spielers
 
 public:
     LottoGame(int count = 6) : numCount(count) // Lotto Game with 6 numbers drawn
     {
-        numbers = new int[numCount];
-        winningNumbers = new int[numCount];
+        numbers = new int[numCount];        // Array für die Zahlen des Spielers
+        winningNumbers = new int[numCount]; // Array für die Gewinnzahlen
     }
-    ~LottoGame()
+    ~LottoGame()    // Destructor
     {
-        delete[] numbers;
-        delete[] winningNumbers;
+        delete[] numbers;           // Löscht das Array der Zahlen des Spielers
+        delete[] winningNumbers;    // Löscht das Array der Gewinnzahlen
     }
-    void getNumbers()
+    void getNumbers() // Eingabe der Zahlen des Spielers
     {
-        cout << "Enter " << numCount << " numbers between 1 and 49:" << endl;
-        for (int i = 0; i < numCount; i++)
+        cout << "Enter " << numCount << " numbers between 1 and 49:" << "\n";   // Aufforderung zur Eingabe der Zahlen
+        for (int i = 0; i < numCount; i++)  // Durchläuft die Zahlen des Spielers
         {
             while (true)
             {
-                cin >> numbers[i];
-                if (numbers[i] >= 1 && numbers[i] <= 49)
+                cin >> numbers[i];  // Eingabe der Zahlen
+                if (numbers[i] >= 1 && numbers[i] <= 49)    // Prüft, ob die Zahl zwischen 1 und 49 liegt
                 {
                     break;
                 }
-                cout << "Invalid number. Please enter a number between 1 and 49:" << endl;
+                cout << "Invalid number. Please enter a number between 1 and 49:" << "\n";
             }
         }
     }
-    void generateNumbers()
+    void generateNumbers() // Generiert die Gewinnzahlen
     {
-        srand(time(0));
-        for (int i = 0; i < numCount; i++)
+        srand(time(0)); // Initialisiert den Zufallsgenerator
+        for (int i = 0; i < numCount; i++)  // Generiert die Gewinnzahlen
         {
-            winningNumbers[i] = rand() % 49 + 1;
-            for (int j = 0; j < i; j++)
+            winningNumbers[i] = rand() % 49 + 1;    // Zufällige Zahl zwischen 1 und 49
+            for (int j = 0; j < i; j++) // Prüft, ob die Zahl bereits generiert wurde
             {
-                if (winningNumbers[i] == winningNumbers[j])
+                if (winningNumbers[i] == winningNumbers[j])  // Falls ja, wird die Zahl erneut generiert
                 {
-                    i--;
-                    break;
+                    i--;    // Dekrementiert i, um die Zahl erneut zu generieren
+                    break;  
                 }
             }
         }
     }
 
-    void printWinningNumbers()
+    void printWinningNumbers() // Druckt die Gewinnzahlen
     {
-        cout << "Winning numbers:" << endl;
+        cout << "Winning numbers:" << "\n";
         for (int i = 0; i < numCount; i++)
         {
             cout << winningNumbers[i] << " ";
         }
-        cout << endl;
+        cout << "\n";
     }
 
-    void printTicket()
+    void printTicket()  // Druckt den Lottoschein des Spielers
     {
-        cout << "Your ticket:" << endl;
-        for (int i = 1; i <= 49; i++)
+        cout << "Your ticket:" << "\n";
+        for (int i = 1; i <= 49; i++)   // Durchläuft die Zahlen von 1 bis 49
         {
-            int count = 0;
-            for (int j = 0; j < numCount; j++)
+            int count = 0;  // Zähler für die Treffer
+            for (int j = 0; j < numCount; j++)  // Durchläuft die Zahlen des Spielers
             {
-                if (i == numbers[j])
+                if (i == numbers[j])    // Prüft, ob die Zahl des Spielers mit der aktuellen Zahl übereinstimmt
                 {
-                    count++;
+                    count++;    // Zählt die Treffer
                 }
             }
-            cout << setw(2) << setfill('0') << i << " (" << count << "), ";
+            cout << setw(2) << setfill('0') << i << " (" << count << "), "; // Druckt die Zahlen und die Anzahl der Treffer, setw und setfill sorgen für die korrekte Ausrichtung
             
-            if (i % 7 == 0) {
-                cout << endl;
+            if (i % 7 == 0) {   // Eine neue Zeile nach 7 Zahlen
+                cout << "\n";
             }
         }
-        cout << endl;
+        cout << "\n";
     }
-    void checkTicket()
+    void checkTicket() // Prüft, ob die Zahlen des Spielers mit den Gewinnzahlen übereinstimmen
     {
-        int matches = 0;
-        for (int i = 0; i < numCount; i++)
+        int matches = 0;    // Zähler für die Treffer
+        for (int i = 0; i < numCount; i++)  // Durchläuft die Zahlen des Spielers
         {
-            for (int j = 0; j < numCount; j++)
+            for (int j = 0; j < numCount; j++)  // Durchläuft die Gewinnzahlen
             {
-                if (numbers[i] == winningNumbers[j])
+                if (numbers[i] == winningNumbers[j])    // Vergleicht die Zahlen
                 {
-                    matches++;
+                    matches++;  // Zählt die Treffer
                 }
             }
         }
-        if (matches == numCount)
+        if (matches == numCount)    // Prüft, ob alle Zahlen übereinstimmen
         {
-            cout << "Congratulations, you won!" << endl;
+            cout << "Congratulations, you won!" << "\n";
         }
-        else
+        else    // Falls nicht, gibt die Anzahl der Treffer aus
         {
-            cout << "Sorry, you didn't win this time. You had " << matches << " matching numbers." << endl;
+            cout << "Sorry, you didn't win this time. You had " << matches << " matching numbers." << "\n";
         }
     }
 };
 
 int main()
 {
-    LottoGame game;
+    LottoGame game; // Erzeugt ein Lotto Spiel
     
-    while (true)
+    while (true)   // "\n"osschleife für das Spiel
     {
-        game.getNumbers();
-        game.generateNumbers();
-        game.printTicket();
-        game.checkTicket();
-        game.printWinningNumbers();
+        game.getNumbers();          // Eingabe der Zahlen des Spielers
+        game.generateNumbers();     // Generiert die Gewinnzahlen
+        game.printTicket();         // Druckt den Lottoschein des Spielers
+        game.checkTicket();         // Prüft, ob die Zahlen des Spielers mit den Gewinnzahlen übereinstimmen
+        game.printWinningNumbers(); // Druckt die Gewinnzahlen
         
         cout << "Do you want to play again? (y/n): ";
-        char answer;
-        cin >> answer;
-        if (answer != 'y')
+        char answer;    // Variable für die Antwort des Spielers
+        cin >> answer;  // Fragt den Spieler, ob er noch einmal spielen möchte
+        if (answer != 'y')  // Beendet das Spiel, falls der Spieler nicht noch einmal spielen möchte
         {
             break;
         }
